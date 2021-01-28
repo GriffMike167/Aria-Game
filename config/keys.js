a7e70309b5d913b866d45a6016f0f8e23d6e9e53
Michael Griffith <griffithcmichael@gmail.com>
2021-01-28 01:06:44 -0600

    begining server set up

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:1KLeGaDxOEGUNhlN@cluster0.1usim.mongodb.net/restaurants?retryWrites=true&w=majority",
+    secretOrKey: "secret",
+    userRoles: ["user", "manager"]
+  };
+  
